5ad6c2a4d460ed35ffe5c1f417cacfa8234de473
Abdullah Sezdi <65413682+AbdullahSezdi@users.noreply.github.com>
2022-01-02 18:56:53 +0300

    upadete the note

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$a.wav');
   }
 
-  Expanded buildkey({Color color,int soundnumber}){
+  Expanded buildkey({Color color,int soundnumber,string note}){
     return Expanded(
       child: TextButton(
         style:TextButton.styleFrom(backgroundColor:color),
@@ -32,13 +32,13 @@ class XylophoneApp extends StatelessWidget {
         body:SafeArea(child:Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            buildkey(color:Colors.red,soundnumber: 1),
-            buildkey(color:Colors.orange,soundnumber: 2),
-            buildkey(color:Colors.yellow,soundnumber: 3),
-            buildkey(color:Colors.green,soundnumber: 4),
-            buildkey(color:Colors.blue,soundnumber: 5),
-            buildkey(color:Colors.indigo,soundnumber: 6),
-            buildkey(color:Colors.purple,soundnumber: 7),
+            buildkey(color:Colors.red,soundnumber: 1,note:"Do"),
+            buildkey(color:Colors.orange,soundnumber: 2,note:"Re"),
+            buildkey(color:Colors.yellow,soundnumber: 3,note:"Mi"),
+            buildkey(color:Colors.green,soundnumber: 4,note:"Fa"),
+            buildkey(color:Colors.blue,soundnumber: 5,note:"Sol"),
+            buildkey(color:Colors.indigo,soundnumber: 6,note:"La"),
+            buildkey(color:Colors.purple,soundnumber: 7,note:"Si"),
             
 
           ],

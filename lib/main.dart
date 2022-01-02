fad67ae22faf8c5a3d66c50ca3002dc1d2ef9de9
Abdullah Sezdi <abdullahsezeies@gmail.com>
2022-01-02 19:02:08 +0300

    updated text-note

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,14 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$a.wav');
   }
 
-  Expanded buildkey({Color color,int soundnumber}){
+  Expanded buildkey({Color color,int soundnumber,String note}){
     return Expanded(
       child: TextButton(
         style:TextButton.styleFrom(backgroundColor:color),
         onPressed: (){
           playsound(soundnumber);
-        },child: Text('Click me',style: TextStyle(color: Colors.white),),
+        },child: Text("$note",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.bold
+      ),),
 
       ),
     );
@@ -32,13 +33,13 @@ class XylophoneApp extends StatelessWidget {
         body:SafeArea(child:Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            buildkey(color:Colors.red,soundnumber: 1),
-            buildkey(color:Colors.orange,soundnumber: 2),
-            buildkey(color:Colors.yellow,soundnumber: 3),
-            buildkey(color:Colors.green,soundnumber: 4),
-            buildkey(color:Colors.blue,soundnumber: 5),
-            buildkey(color:Colors.indigo,soundnumber: 6),
-            buildkey(color:Colors.purple,soundnumber: 7),
+            buildkey(color:Colors.red,soundnumber: 1,note:"Do"),
+            buildkey(color:Colors.orange,soundnumber: 2,note:"Re"),
+            buildkey(color:Colors.yellow,soundnumber: 3,note:"Mi"),
+            buildkey(color:Colors.green,soundnumber: 4,note:"Fa"),
+            buildkey(color:Colors.blue,soundnumber: 5,note:"Sol"),
+            buildkey(color:Colors.indigo,soundnumber: 6,note:"La"),
+            buildkey(color:Colors.purple,soundnumber: 7,note:"Si"),
             
 
           ],
